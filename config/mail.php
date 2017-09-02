cc279f34607f789f82134cbf5cf5b494d3e60def
charles0830 <bhgold830@gmail.com>
2017-09-02 22:30:39 +0600

    config change heroku

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'tapos.aa@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'info@tutexp.com'),
         'name' => env('MAIL_FROM_NAME', 'EVeryOne Journalist'),
     ],
 
