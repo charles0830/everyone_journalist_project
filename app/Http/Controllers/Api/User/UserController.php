2c2946893b27a7ed627c61a3af4adb69529d95b5
charles0830 <bhgold830@gmail.com>
2017-09-18 22:19:03 +0600

    resend email error solve

--- app/Http/Controllers/Api/User/UserController.php
+++ app/Http/Controllers/Api/User/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\User;
 
 use App\Http\Controllers\ApiController;
+use App\Mail\UserCreated;
 use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
